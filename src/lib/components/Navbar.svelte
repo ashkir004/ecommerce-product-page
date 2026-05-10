eb19eab0a43bae16e77f99dad2328f53fc5e7c05
ashkir004 <ashkir004@gmail.com>
2026-05-11 00:05:44 +0300

    implemented menu

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,5 +1,6 @@
 <script>
     import menu from '$lib/assets/images/icon-menu.svg';
+    import closeMenu from '$lib/assets/images/icon-close.svg';
     import logo from '$lib/assets/images/logo.svg';
     import cart from '$lib/assets/images/icon-cart.svg';
     import avatar from '$lib/assets/images/image-avatar.png';
@@ -7,15 +8,25 @@
     let activeTab = $state('Women');
     let tabs = ['Collections', 'Men', 'Women', 'About', 'Contact'];
 
+    let isMenuOpen = $state(false);
+
 </script>
 
 
 <header>
     <div class="menu_log-wrapper">
-        <img src={menu} alt="Menu Icon" class="menu show-sm hide-lg" />
+        <button onclick={() => isMenuOpen = true}>
+            <img src={menu} alt="Menu Icon" 
+                class="menu {isMenuOpen ? 'hide-sm' : 'show-sm'} hide-lg" />
+        </button>
         <img src={logo} alt="Ecommerce Product Page Logo" class="logo" />
     </div>
-    <nav class="hide-sm show-lg">
+    <nav class="{isMenuOpen ? 'show-sm' : 'hide-sm'} show-lg">
+        <button
+            onclick={() => isMenuOpen = false}
+            class="close-menu {isMenuOpen ? 'show-sm' : 'hide-sm'} hide-lg">
+            <img src={closeMenu} alt="Close Menu Icon" />
+        </button>
         <ul>
             {#each tabs as tab, index (index)}
                 <li class={tab === activeTab ? 'active' : ''}>
@@ -46,37 +57,45 @@
     }
 
     nav {
-        padding: 0;
-        align-self: stretch;
+        background-color: var(--white);
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 70%;
+        height: 100vh;
         display: flex;
-        flex-direction: row;
-        align-items: flex-end;
+        flex-direction: column;
+        align-items: flex-start;
+        gap: var(--space-700);
+        padding: var(--space-400) var(--space-300) var(--space-300);
+        z-index: 999;
+        box-shadow: 0 2px 10px -2px var(--grey-500);
+    }
+
+    button {
+        background: none;
+        border: none;
+        padding: 0;
+        cursor: pointer;
     }
 
     nav ul {
         display: flex;
-        flex-direction: row;
-        gap: var(--space-400);
-        height: 100%;
-    }
-    
-    nav ul li {
-        list-style: none;
-        display: flex;
-        align-items: center;
-        height: 100%;
-    }
-    
-    nav ul li a {
-        color: var(--grey-500);
-        text-decoration: none;
-        font-size: var(--font-sm);
-        font-weight: var(--w-regular);
-        line-height: var(--line-height-lg);
+        flex-direction: column;
+        gap: var(--space-300);
     }
 
-    .active {
-        border-bottom: 2px solid var(--orange-500);
+    nav ul li {
+        list-style: none;
+        color: var(--grey-950);
+    }
+
+    nav ul li a {
+        color: var(--grey-950);
+        text-decoration: none;
+        font-size: var(--font-md);
+        font-weight: var(--w-bold);
+        line-height: var(--line-height-xl);
     }
 
     .hide-sm {
@@ -84,7 +103,7 @@
     }
 
     .show-sm {
-        display: block;
+        display: flex;
     }
 
     .menu_log-wrapper {
@@ -117,6 +136,11 @@
             padding-inline: 0;
         }
 
+        nav {
+            padding-left: var(--space-1100);
+            padding-top: var(--space-600);
+        }
+
         .cart_avatar-wrapper {
             gap: var(--space-600);
             
@@ -136,8 +160,44 @@
             grid-column: 1 / 3;
         }
 
+        nav {
+            position: static;
+            width: auto;
+            height: auto;
+            background-color: transparent;
+            box-shadow: none;
+            padding: 0;
+            align-self: stretch;
+            display: flex;
+            flex-direction: row;
+            align-items: flex-end;
+        }
+        
         nav ul {
+            display: flex;
+            flex-direction: row;
+            gap: var(--space-400);
+            height: 100%;
             margin-bottom: 4rem;
+        }
+        
+        nav ul li {
+            list-style: none;
+            display: flex;
+            align-items: center;
+            height: 100%;
+        }
+        
+        nav ul li a {
+            color: var(--grey-500);
+            text-decoration: none;
+            font-size: var(--font-sm);
+            font-weight: var(--w-regular);
+            line-height: var(--line-height-lg);
+        }
+
+        .active {
+            border-bottom: 2px solid var(--orange-500);
         }
 
         .cart_avatar-wrapper {
